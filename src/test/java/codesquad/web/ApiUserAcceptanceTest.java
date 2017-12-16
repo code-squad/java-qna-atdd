d4bfae7aaf981fa284ace7753a81a5f4af77aa93
javajigi <javajigi@gmail.com>
2017-12-16 16:45:05 +0900

    수정 기능 추가함.

--- src/test/java/codesquad/web/ApiUserAcceptanceTest.java
+++ src/test/java/codesquad/web/ApiUserAcceptanceTest.java
@@ -1,7 +1,7 @@
 package codesquad.web;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -14,7 +14,7 @@ import support.test.AcceptanceTest;
 public class ApiUserAcceptanceTest extends AcceptanceTest {
 
     @Test
-    public void create_user() throws Exception {
+    public void create() throws Exception {
         UserDto newUser = createUserDto("testuser1");
         String location = createResource("/api/users", newUser);
         
@@ -24,7 +24,7 @@ public class ApiUserAcceptanceTest extends AcceptanceTest {
     
     @Test
     public void show_다른_사람() throws Exception {
-        UserDto newUser = createUserDto("testuser3");
+        UserDto newUser = createUserDto("testuser2");
         String location = createResource("/api/users", newUser);
         
         ResponseEntity<String> response = getResource(location, findDefaultUser());
@@ -36,8 +36,8 @@ public class ApiUserAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
-    public void update_user() throws Exception {
-        UserDto newUser = createUserDto("testuser2");
+    public void update() throws Exception {
+        UserDto newUser = createUserDto("testuser3");
         String location = createResource("/api/users", newUser);
         
         User loginUser = findByUserId(newUser.getUserId());
@@ -46,5 +46,17 @@ public class ApiUserAcceptanceTest extends AcceptanceTest {
         
         UserDto dbUser = getResource(location, UserDto.class, findByUserId(newUser.getUserId()));
         assertThat(dbUser, is(updateUser));
+    }
+    
+    @Test
+    public void update_다른_사람() throws Exception {
+        UserDto newUser = createUserDto("testuser4");
+        String location = createResource("/api/users", newUser);
+        
+        UserDto updateUser = new UserDto(newUser.getUserId(), "password", "name2", "javajigi@slipp.net2");
+        basicAuthTemplate(findDefaultUser()).put(location, updateUser);
+        
+        UserDto dbUser = getResource(location, UserDto.class, findByUserId(newUser.getUserId()));
+        assertThat(dbUser, is(newUser));
     }
 }
